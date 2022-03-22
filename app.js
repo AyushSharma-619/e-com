c3ab19d6a5f54921f052e9c9be939e36eeec6f7d
aj <asharma619@rediffmail.com>
2022-03-23 00:27:20 +0530

    added mongoose

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+
+const mongoose = require('mongoose')
 const User = require('./models/user')
 
 const app = express();
@@ -14,14 +15,15 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { getMaxListeners } = require('process');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('622b8c9f2347e4123b751f77')
+  User.findById('623a04b8d6327533e8a4a069')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err)); 
@@ -32,7 +34,23 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect( ()=>{
-  console.log()
+mongoose.connect('mongodb+srv://ayush:aZNSrLNh6ZqR8Vbb@cluster0.clqvv.mongodb.net/shop?retryWrites=true&w=majority')
+.then((result)=>{
+  User.findOne().then((user)=>{
+    if(!user){
+      const user = new User({
+        name:'Ayush',
+        email:'asharma619@gmail.com',
+        cart:{
+          items:[]
+        }
+      })
+      user.save()
+    }
+    
+  })
+  
   app.listen(3000)
+}).catch((err)=>{
+  console.log(err)
 })
